9f91d2b68ae12703905bf8985ce5e382dad0e7d2
SlavaHoBu4eK <newbogdanov@gmail.com>
2022-08-25 23:06:08 +0300

    add function template in the index file

--- common.php
+++ common.php
@@ -11,3 +11,54 @@ $commentRepository = new CommentRepository($connection);
 function escape($html) {
     return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE,"UTF-8");
 }
+
+
+function template($file, $args){
+    if ( file_exists('/var/www/html/templates/'.$file.'.tpl.php')) {   //проверка существует ли файл
+        ob_start(); // откываем буфер обмена
+        extract($args);
+        require 'templates/'.$file.'.tpl.php';
+        return ob_get_clean(); //очищаем буфер обмена
+    }
+}
+
+//function template( $file, $args ){
+//    if ( !file_exists( $file ) ) {
+//        return '';
+//    }
+//    if ( is_array( $args ) ){
+//        extract( $args );
+//    }
+//    ob_start();
+//    include $file;
+//    return ob_get_clean();
+//}
+
+
+//function template($names, $clients)
+//{
+//    if (!is_array($names)) {
+//        $names = array($names);
+//    }
+//    $template_found = false;
+//    foreach ( $names as $name ) {
+//        $file ='/var/www/html/templates/'.$name.'.tpl.php';
+//        if ( file_exists( $file ) ) {
+//            $template_found = $file;
+//            // stop after the first template is found
+//            break;
+//        }
+//    }
+//    // fail if no template file is found
+//    if ( ! $template_found ) {
+//        return '';
+//    }
+//    // Make values in the associative array easier to access by extracting them
+//    if ( is_array( $clients ) ){
+//        extract( $clients );
+//    }
+//    // buffer the output (including the file is "output")
+//    ob_start();
+//    include $template_found;
+//    return ob_get_clean();
+//}
--- public/index.php
+++ public/index.php
@@ -6,63 +6,12 @@ include '../src/entity/client.php';
 require_once "../common.php";
 include '../templates/header.php';
 
-
 ?>
-    <h3>Добавление нового клиента</h3>
-    <form action="create.php" method="post">
-        <label>Фамилия:</label>
-        <label>
-            <input type="text" name="last_name" placeholder="Введите фамилию">
-        </label>
-        <label>Имя:</label>
-        <label>
-            <input type="text" name="name" placeholder="Введите имя">
-        </label>
-        <label>Отчество:</label>
-        <label>
-            <input type="text" name="middle_name" placeholder="Введите отчество">
-        </label>
-        <label>Дата рождения:</label>
-        <label>
-            <input type="date" name="birthday" placeholder="Введите дату рождения">
-        </label>
-        <label>Номер телефона:</label>
-        <label>
-            <input type="text" name="phone" placeholder="Введите номер телефона">
-        </label>
-        <button type="submit" name="submit">Добавить нового клиента</button>
-    </form>
 
-    <br><br>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Фамилия</th>
-            <th>Имя</th>
-            <th>Отчество</th>
-            <th>Дата рождения</th>
-            <th>Номер телефона</th>
-            <th>Редактирование</th>
-            <th>Удаление</th>
-            <th>Просмотр</th>
-        </tr>
+<?php $args = $clientRepository->findAll();?>
 
-        <?php
-        $sn = 1;
-        $clients = $clientRepository->findAll();
-        foreach ($clients as $client) { ?>
-            <tr>
-                <td><?= $sn++ ?>.</td>
-                <td><?= $client->getLastName() ?></td>
-                <td><?= $client->getName() ?></td>
-                <td><?= $client->getMiddleName() ?></td>
-                <td><?= $client->getBirthday() ?></td>
-                <td><?= $client->getPhone() ?></td>
-                <td><a href="update.php?id=<?= $client->getId() ?>">Редактировать</a></td>
-                <td><a href="delete.php?id=<?= $client->getId() ?>">Удалить</a></td>
-                <td><a href="view.php?id=<?= $client->getId() ?>">Просмотреть</a></td>
-            </tr>
-        <?php } ?>
-    </table>
+<?= template('index', $args) ?>
 
 <?php include '../templates/footer.php'; ?>
+
+
